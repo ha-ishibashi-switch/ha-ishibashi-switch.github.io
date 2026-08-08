269a869d4796c56792931f4885d4e5549ac0e33f
ha-ishibashi-switch <ha-ishibashi@switch-ang.co.jp>
2026-08-08 18:30:32 +0900

    ルーティング修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Survey from "./pages/Survey";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey" element={<Survey />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
